73a2dc783005bc48e1c520372b816630c5d7121c
GuanSuns <guansuns@outlook.com>
2017-06-19 17:34:49 +0800

    Update fillRowPercentage

--- src/main/java/config/ExcelConfig.java
+++ src/main/java/config/ExcelConfig.java
@@ -7,6 +7,7 @@ import org.apache.poi.ss.usermodel.IndexedColors;
  * Created by guanl on 6/19/2017.
  */
 public class ExcelConfig {
+    //Sheet names settings
     public static String SheetName411 = "4.1.1应用OS文件系统目录使用率";
     public static String SheetName421 = "4.2.1数据库OS文件系统目录使用率";
     public static String SheetName422 = "4.2.2表空间使用率检查";
@@ -18,13 +19,21 @@ public class ExcelConfig {
     public static String SheetName428 = "4.2.8时钟同步检查";
     public static String SheetName429 = "4.2.9集群心跳检查";
 
+    //Default cell style
     public static boolean cellBorder = true;
-    public static boolean centralAlginment = true;
-
+    public static boolean centralAlignment = true;
     public static String font = "等线";
     public static short fontSize = 220;
-    public static BorderStyle borderStyle = BorderStyle.MEDIUM;
+    public static BorderStyle borderStyle = BorderStyle.THIN;
     public static short borderColor = IndexedColors.BLACK.index;
 
+    //Default value for null
+    public static String nullValue = "无";
+
+    //Sheet 4.1.1 Setting
+    public static int Sheet411PersonalStart = 3;
+    public static int Sheet411PersonalEnd = 15;
+    public static int Sheet411CoreStart = 3;
+    public static int Sheet411CoreEnd = 17;
 
 }
