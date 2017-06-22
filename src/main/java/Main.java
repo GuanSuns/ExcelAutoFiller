d44d9814ae0923a1187a5a57b919b7b770941add
GuanSuns <guansuns@outlook.com>
2017-06-22 13:44:58 +0800

    Update fillRowPercentage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,20 @@
-import config.ExcelConfig;
 import config.FileDestination;
-import model.Sheet411PDM;
+import model.Sheet411CorePDM;
+import model.Sheet411PersonalPDM;
+import model.Sheet421PersonalPDM;
 import sheet.filler.Sheet411Filler;
+import sheet.filler.Sheet421Filler;
 
 public class Main {
 
     public static void main(String[] args) {
         try{
-            Sheet411PDM sheet411PDM = new Sheet411PDM(null, 1L
-                    , 0.112F, null, null
-                    , 1F, 0.1233F, 1F
-                    , 1F, 1F, 1F, 1F
-                    , 1F, 1F, 1F, 1F);
+            Sheet421PersonalPDM sheet421PersonalPDM = new Sheet421PersonalPDM(null, null, null
+                    , 0.23F, 0.23f, 0.23F
+                    , 0.34F, 0.4554F, 0.112F
+                    , 0.56F, 0.5432F, 0.0012F);
 
-            Sheet411Filler.fill(sheet411PDM, FileDestination.Personal);
+            Sheet421Filler.fill(sheet421PersonalPDM, FileDestination.Personal);
         }catch (Exception e){
             e.printStackTrace();
         }
